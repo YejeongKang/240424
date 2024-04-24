96455261a274e6c441bc8f8c530661aea2fb7912
YejeongKang <dpwjd1130@gmail.com>
2024-04-24 15:40:36 +0900

    Add project files.

---
+++ 240424/func.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int GetTen()
+{
+    //GetFive(); //깊이 두 단계 들어가진 것.
+    return 10;
+}
+
+int IntSum(int n1, int n2)
+{
+    return n1 + n2;
+}
+
+int main()
+{
+    // cin >> num1 >> num2; 
+    // 두 개 동시에 입력받을 수 있음.
+
+    int ten = GetTen(); // ten = 10;
+    int result = IntSum(4, 7); // 4 + 7
+    // int result = PrintHi(); // 리턴되는 값이 없어서 가져올 것도 없다.
+    PrintSomething("Neuron");
+
+}
+void PrintSomething(string message)
+{
+    cout << message << endl;
+}
+
+void PrintHi()
+{
+    cout << "Hi ~~~" << endl;
+}
+
---
+++ 240424/prac1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int n1, int n2) {
+	return n1 + n2;
+}
+
+int sub(int n1, int n2) {
+	return abs(n1 - n2);
+}
+
+int mul(int n1, int n2) {
+	return n1 * n2;
+}
+
+float devide(int n1, int n2) {
+	return float(n1) / float(n2);
+}
+
+int main() {
+	int n1, n2; 
+	cout << "정수 두 개를 입력하시오.\n";
+	cin >> n1 >> n2;
+
+	cout << add(n1, n2) << endl;
+	cout << sub(n1, n2) << endl;
+	cout << mul(n1, n2) << endl;
+	cout << devide(n1, n2) << endl;
+
+}
