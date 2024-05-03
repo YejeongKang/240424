5470b7ab64b7c904b18895187138ece23728df57
YejeongKang <dpwjd1130@gmail.com>
2024-05-03 21:34:59 +0900

    FileName

--- 240424/prac2_find_country.cpp
+++ 240424/prac2_find_country.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 int main() {
 	string city[5] = { "Korea", "America", "France", "Germany", "Japan" };
-	string nation;
+	string input;
 	bool check = false;
 	int CityArraySize = sizeof(city) / sizeof(city[0]);
 	
 	cout << "찾는 나라가 어딘교 (모두 출력: list): " << endl;
-	cin >> nation;
+	cin >> input;
 		
-	if (nation == "list") {
-		for (string nation : city) {
-			cout << nation << endl;
+	if (input == "list") {
+		for (string input : city) {
+			cout << input << endl;
 		}
 	}
 	else {
 		for (int i = 0; i < CityArraySize; i++) {
-			if (nation == city[i]) {
+			if (input == city[i]) {
 				check = true;
 				cout << "[" << i + 1 << "]" << city[i] << endl;
 				break;
